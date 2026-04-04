b3f296099ead7cbcc08d720ad970e71502229680
zenos <zenos.chen@foxmail.com>
2026-04-05 01:22:54 +0800

    feat: 添加basePath

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingRoot: path.join(__dirname, ".."),
+  basePath: "/person-site",
   env: {
     NEXT_PUBLIC_API_BASE_PATH: process.env.NEXT_PUBLIC_API_BASE_PATH ?? "/api/v1",
   },
